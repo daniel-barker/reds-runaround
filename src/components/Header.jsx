f2221c5e71458916482788c0b5a32d6be5bdec7a
dan barker <dbarker16@gmail.com>
2023-12-06 18:04:48 -0500

    header fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,17 +5,6 @@ import { Image, Navbar, Nav } from "react-bootstrap";
 const Header = () => {
   return (
     <>
-      <Navbar expand="lg" className="text-center">
-        <Nav className="w-100 d-flex justify-content-between">
-          <Nav.Link href="about">About Us</Nav.Link>
-          <Nav.Link href="services">Our Services</Nav.Link>
-          <Nav.Link href="faqs">FAQs</Nav.Link>
-          <Nav.Link href="safety">Safety & Rules</Nav.Link>
-          <Nav.Link href="parks">Parks</Nav.Link>
-          <Nav.Link href="contact">Contact Us</Nav.Link>
-          {/* <Nav.Link href="gallery">Happy Tails & Highlights</Nav.Link> */}
-        </Nav>
-      </Navbar>
       <Nav.Link className="text-center" href="/">
         <Image
           src={logo}
@@ -24,6 +13,17 @@ const Header = () => {
           fluid
         />
       </Nav.Link>
+      <Navbar expand="lg" className="text-center">
+        <Nav className="w-100 d-flex justify-content-between">
+          <Nav.Link href="/about">About Us</Nav.Link>
+          <Nav.Link href="/services">Our Services</Nav.Link>
+          <Nav.Link href="/faqs">FAQs</Nav.Link>
+          <Nav.Link href="/safety">Safety & Rules</Nav.Link>
+          <Nav.Link href="/parks">Parks</Nav.Link>
+          <Nav.Link href="/contact">Contact Us</Nav.Link>
+          {/* <Nav.Link href="gallery">Happy Tails & Highlights</Nav.Link> */}
+        </Nav>
+      </Navbar>
     </>
   );
 };
